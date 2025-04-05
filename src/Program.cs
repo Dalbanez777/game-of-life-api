4fd9d7283c99ccde7741c4f6bb9d424cb09c31b7
Daniel albanez <daniel7albanez@gmail.com>
2025-04-04 23:35:03 -0600

    Add global exception handler for error concealment

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using GameOfLife.Services;
 
 
@@ -18,6 +21,24 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<GameOfLifeService>();
 
 var app = builder.Build();
+
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "application/json";
+
+        var errorFeature = context.Features.Get<IExceptionHandlerFeature>();
+        if (errorFeature != null)
+        {
+            var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+            logger.LogError(errorFeature.Error, "Unhandled exception");
+
+            await context.Response.WriteAsJsonAsync(new { error = "An unexpected error occurred. Please try again later." });
+        }
+    });
+});
 
 app.UseSwagger();
 app.UseSwaggerUI();
